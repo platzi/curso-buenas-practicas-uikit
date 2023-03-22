4da80fe45483a9e4a9b8d551eb1276356a668fe5
René Sandoval <resand91@gmail.com>
2023-03-21 22:48:11 -0600

    UIKit configuration for initial project
    Signed-off-by: René Sandoval <resand91@gmail.com>
    
    	new file:   .gitignore
    	new file:   LICENSE
    	new file:   UIKit iOS Platzi.xcodeproj/project.pbxproj
    	new file:   UIKit iOS Platzi/AppDelegate.swift
    	new file:   UIKit iOS Platzi/Assets.xcassets/AccentColor.colorset/Contents.json
    	new file:   UIKit iOS Platzi/Assets.xcassets/AppIcon.appiconset/Contents.json
    	new file:   UIKit iOS Platzi/Assets.xcassets/Contents.json
    	new file:   UIKit iOS Platzi/Base.lproj/LaunchScreen.storyboard
    	new file:   UIKit iOS Platzi/Info.plist
    	new file:   UIKit iOS Platzi/ViewController.swift

---
+++ UIKit iOS Platzi/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  UIKit iOS Platzi
+//
+//  Created by René Sandoval on 21/03/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
---
+++ UIKit iOS Platzi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit iOS Platzi
+//
+//  Created by René Sandoval on 21/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
