64ba998e011451f9e9b77675fbf48a7052355484
René Sandoval <resand91@gmail.com>
2023-03-22 18:44:22 -0600

    TableView added
    Signed-off-by: René Sandoval <resand91@gmail.com>
    
    Changes to be committed:
    	modified:   UIKit iOS Platzi.xcodeproj/project.pbxproj
    	modified:   UIKit iOS Platzi/AppDelegate.swift
    	new file:   UIKit iOS Platzi/Assets.xcassets/platziSpace.imageset/Contents.json
    	new file:   UIKit iOS Platzi/Assets.xcassets/platziSpace.imageset/Space-Program.png
    	new file:   UIKit iOS Platzi/MainTableViewCell.swift
    	new file:   UIKit iOS Platzi/MainTableViewController.swift

---
+++ UIKit iOS Platzi/MainTableViewController.swift
@@ -0,0 +1,71 @@
+//
+//  MainTableViewController.swift
+//  UIKit iOS Platzi
+//
+//  Created by René Sandoval on 22/03/23.
+//
+
+import UIKit
+
+class MainTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    private let titleTable: UILabel = UILabel()
+    private let tableView: UITableView = UITableView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+
+        setupView()
+        setupLayout()
+    }
+
+    private func setupView() {
+        /// Title label
+        titleTable.translatesAutoresizingMaskIntoConstraints = false
+        titleTable.text = "TableView"
+        titleTable.textColor = .systemGreen
+        titleTable.textAlignment = .center
+        titleTable.font = .systemFont(ofSize: 24, weight: .bold)
+
+        /// TableView
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.separatorStyle = .none
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(MainTableViewCell.self, forCellReuseIdentifier: MainTableViewCell.id)
+    }
+
+    private func setupLayout() {
+        view.addSubview(titleTable)
+        view.addSubview(tableView)
+
+        NSLayoutConstraint.activate([
+            titleTable.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+            titleTable.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 24),
+            titleTable.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
+            titleTable.heightAnchor.constraint(equalToConstant: 40),
+
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.topAnchor.constraint(equalTo: titleTable.bottomAnchor, constant: 12),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MainTableViewCell.id, for: indexPath) as? MainTableViewCell else {
+            fatalError("Could not cast MainTableViewCell")
+        }
+
+        cell.setup(name: "Nombre", image: UIImage(named: "platziSpace")!)
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+}
